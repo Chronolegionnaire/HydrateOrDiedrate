9610a78d34fd08a767123f6c7432c36705fcdcc0
_Kristof_ <krizziewissie@gmail.com>
2025-09-21 13:48:07 +0200

    Allow for extracting liquid from bucket without taking it off

--- HydrateOrDiedrate/src/Wells/Winch/BlockWinch.cs
+++ HydrateOrDiedrate/src/Wells/Winch/BlockWinch.cs
@@ -2,6 +2,7 @@
 using Vintagestory.API.MathTools;
 using Vintagestory.API.Client;
 using Vintagestory.GameContent.Mechanics;
+using Vintagestory.GameContent;
 
 namespace HydrateOrDiedrate.Wells.Winch;
 
@@ -27,7 +28,18 @@ public class BlockWinch : BlockMPBase
             {
                 case 0:
                     var sourceSlot = byPlayer.InventoryManager.ActiveHotbarSlot;
-                    if(sourceSlot is not null && (sourceSlot.Empty || beWinch.InputSlot.Empty) && sourceSlot.TryFlipWith(beWinch.InputSlot)) return true;
+                    if(sourceSlot is not null)
+                    {
+                        if((sourceSlot.Empty != beWinch.InputSlot.Empty) && sourceSlot.TryFlipWith(beWinch.InputSlot)) return true;
+
+                        if(TryTransferLiquid(beWinch.InputSlot.Itemstack, sourceSlot.Itemstack))
+                        {
+                            sourceSlot.MarkDirty();
+                            beWinch.InputSlot.MarkDirty();
+                            return true;
+                        }
+                    }
+
                     break;
 
                 case 1:
@@ -37,6 +49,25 @@ public class BlockWinch : BlockMPBase
         }
         
         return base.OnBlockInteractStart(world, byPlayer, blockSel);
+    }
+
+    public static bool TryTransferLiquid(ItemStack source, ItemStack target)
+    {
+        if(source?.Collectible is not BlockLiquidContainerBase sourceContainer || target?.Collectible is not BlockLiquidContainerBase targetContainer) return false;
+        var existingLiters = targetContainer.GetCurrentLitres(target);
+        var remainingSpace = targetContainer.CapacityLitres - existingLiters;
+        if(remainingSpace <= 0) return false;
+
+        var existingContent = targetContainer.GetContent(target);
+        var newContent = sourceContainer.GetContent(source);
+        if(newContent is null || (existingContent is not null && existingContent.Collectible.Code != newContent.Collectible.Code)) return false;
+
+        var addedLiquid = sourceContainer.TryTakeLiquid(source, remainingSpace);
+        if(addedLiquid is null) return false;
+        if(existingContent is not null) addedLiquid.StackSize += existingContent.StackSize;
+
+        targetContainer.SetContent(target, addedLiquid);
+        return true;
     }
 
     public override bool OnBlockInteractStep(float secondsUsed, IWorldAccessor world, IPlayer byPlayer, BlockSelection blockSel)
