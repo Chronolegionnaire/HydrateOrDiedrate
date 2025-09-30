50bae6decb7ad96b74d6d6dac6a30599db3b5f9f
Chronolegionnaire <bryantbakeraz@gmail.com>
2025-09-30 12:52:58 -0700

    I totally know how to code

--- HydrateOrDiedrate/src/Thirst/EntityBehaviorThirst.cs
+++ HydrateOrDiedrate/src/Thirst/EntityBehaviorThirst.cs
@@ -163,28 +163,18 @@ public partial class EntityBehaviorThirst(Entity entity) : EntityBehavior(entity
             thirstDecayRate *= config.SprintThirstMultiplier;
         }
 
-        foreach (var modifier in player.SidedProperties.Behaviors.OfType<IThirstRateModifier>())
+        foreach(var modifier in player.SidedProperties.Behaviors.OfType<IThirstRateModifier>())
         {
             var newThirstDecayRate = modifier.OnThirstRateCalculate(thirstDecayRate);
-
+            
             if (float.IsFinite(newThirstDecayRate)) thirstDecayRate = newThirstDecayRate;
         }
 
         thirstDecayRate = Math.Min(thirstDecayRate, config.ThirstDecayRate * config.ThirstDecayRateMax);
 
-        if (storySys != null)
+        if(storySys?.GetStoryStructureAt(entity.SidedPos.AsBlockPos) is not null)
         {
-            float storyMul = ModConfig.Instance.Thirst.ThirstRateAtStoryLocations;
-            if (storyMul < 1f && player != null)
-            {
-                var pos = player.ServerPos;
-                bool inStory = storySys.GetStoryStructureAt(new BlockPos((int)pos.X, (int)pos.Y, (int)pos.Z)) != null;
-                if (inStory)
-                {
-                    float cap = ModConfig.Instance.Thirst.ThirstDecayRate * Math.Max(0f, storyMul);
-                    if (thirstDecayRate > cap) thirstDecayRate = cap;
-                }
-            }
+            thirstDecayRate *= GameMath.Clamp(ModConfig.Instance.Thirst.ThirstRateAtStoryLocations, 0, 2);
         }
 
         if (entity.World.ElapsedMilliseconds - lastMoveMs > 3000L) thirstDecayRate *= config.IdleThirstModifier;
