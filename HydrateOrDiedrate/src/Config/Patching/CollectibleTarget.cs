28cca909b5537481fbf91c5dd88cf9077421538c
Chronolegionnaire <bryantbakeraz@gmail.com>
2026-01-29 10:13:24 -0700

    fix patching wildcard bug

--- HydrateOrDiedrate/src/Config/Patching/CollectibleTarget.cs
+++ HydrateOrDiedrate/src/Config/Patching/CollectibleTarget.cs
@@ -10,49 +10,57 @@ public class CollectibleTarget
     private readonly string domain;
     private readonly string pathPrefix;
     private readonly Regex pathRemainderRexeg;
+    private readonly bool exactMatch;
 
     public CollectibleTarget(ReadOnlySpan<char> code)
     {
-        var index = code.IndexOf(':'); //Extract domain
+        var index = code.IndexOf(':');
         if (index != -1)
         {
-            if(index != 1 || code[0] != '*') //But only if domain is not a wildcard
+            if (!(index == 1 && code[0] == '*'))
             {
                 domain = code[..index].ToString();
             }
             code = code[(index + 1)..];
         }
 
-        index = code.IndexOf('*'); //Extract until first wildcard
-        if(index != -1)
+        index = code.IndexOf('*');
+        if (index != -1)
         {
             pathPrefix = code[..index].ToString();
             code = code[index..];
+
+            if (code.IsEmpty || (code.Length == 1 && code[0] == '*')) return;
+
+            pathRemainderRexeg = new Regex(
+                $"^{Regex.Escape(code.ToString()).Replace("\\*", ".*")}$",
+                RegexOptions.Compiled
+            );
         }
         else
         {
-            //No wildcards used
+            // No wildcards used => EXACT match
+            exactMatch = true;
             pathPrefix = code.ToString();
-            return;
         }
-
-        if(code.IsEmpty || (code.Length == 1 && code[0] == '*')) return;
-        
-        //If more remained then we have to create a regex
-        pathRemainderRexeg = new Regex($"^{Regex.Escape(code.ToString()).Replace("\\*", ".*")}$", RegexOptions.Compiled);
     }
 
     public bool IsMatch(CollectibleObject collectible)
     {
         var code = collectible.Code;
-        if(code is null || (domain is not null && code.Domain != domain)) return false;
+        if (code is null || (domain is not null && code.Domain != domain)) return false;
+
         ReadOnlySpan<char> path = code.Path;
 
-        if(!path.StartsWith(pathPrefix)) return false;
+        if (exactMatch)
+        {
+            // must be identical, not "starts with"
+            return path.SequenceEqual(pathPrefix);
+        }
+
+        if (!path.StartsWith(pathPrefix)) return false;
         path = path[pathPrefix.Length..];
 
-        if(pathRemainderRexeg is null) return true;
-        
-        return pathRemainderRexeg.IsMatch(path);
+        return pathRemainderRexeg is null || pathRemainderRexeg.IsMatch(path);
     }
 }
